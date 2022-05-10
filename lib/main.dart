1cd50ee86c3c221631af9ebd7e9f236e7414870a
JAtoms <joshua.nwokoye.g20@gmail.com>
2022-05-10 02:01:13 +0100

    Routes, enums and constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,25 @@
-import 'package:bt_commerce/constants/colors.dart';
-import 'package:bt_commerce/ui/dashboard/dashboard.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import 'constants/size_config.dart';
+import 'package:flutter/material.dart';
+import 'routes.dart';
+import 'screens/splash/splash_screen.dart';
+import 'theme.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-      ),
-      child: LayoutBuilder(builder: (context, constraints) {
-        return OrientationBuilder(builder: (context, orientation) {
-          SizeConfig().init(constraints, orientation);
-          return MaterialApp(
-            title: 'Bt-Commerce',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-                primarySwatch: GlobalColors.materialPrimaryColor,
-                textTheme:
-                    GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)),
-            home: const Dashboard(),
-          );
-        });
-      }),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: theme(),
+      // home: SplashScreen(),
+      // We use routeName so that we dont need to remember the name
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
     );
   }
 }
